d3f1c1ac33c593840e5424ec6ff59890f7982114
MakoThoth <robjcbrown@gmail.com>
2026-08-06 07:42:06 -0700

    RESTORE: Recover api/test/index.ts deleted in commit d4b81e2

---
+++ api/test/index.ts
@@ -0,0 +1,6 @@
+import { VercelRequest, VercelResponse } from '@vercel/node';
+export default async function handler(req: VercelRequest, res: VercelResponse) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Content-Type', 'application/json');
+  return res.status(200).json({ test: true, message: "Test endpoint works" });
+}
