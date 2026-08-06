5200cb8e9d61d4daaa965ca7f8b741a90720a58f
MakoThoth-KClaw <makothoth@athelgard.io>
2026-08-06 14:26:13 +0800

    fix: Remove broken api/github, fix vercel.json JSON syntax, use unified router

--- api/github/index.js
+++
@@ -1,5 +0,0 @@
-module.exports = function handler(req, res) {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
-  res.status(200).json({ status: 'ok', message: 'GitHub endpoint working' });
-};
